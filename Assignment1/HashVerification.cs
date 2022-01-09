c7edc93d6e74b7384acbf4a33ab27bbbb189eb3f
s3813866 <tehyeuhaw@gmail.com>
2022-01-09 18:39:51 +1100

    Implemented SimpleHashing library

---
+++ Assignment1/HashVerification.cs
@@ -0,0 +1,10 @@
+﻿namespace Main;
+using SimpleHashing;
+
+public static class HashVerification
+{
+    public static bool PasswordCheck(string hash, string inputPw)
+    {
+        return PBKDF2.Verify(hash, inputPw);
+    }
+}
