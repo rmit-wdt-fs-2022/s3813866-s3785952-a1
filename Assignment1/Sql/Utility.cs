57b86591a2b5e52525e72d1c91c5c364ba13e6e2
s3813866 <tehyeuhaw@gmail.com>
2022-01-10 18:44:22 +0800

    Feat: Seeding data methods are implemented and minor type change in Customer and Transaction

---
+++ Assignment1/Sql/Utility.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace Main.Sql;
+
+public static class Utility
+{
+    public static DataTable GetDataTable(this SqlCommand command)
+    {
+        using var adapter = new SqlDataAdapter(command);
+    
+        var table = new DataTable();
+        adapter.Fill(table);
+    
+        return table;
+    } 
+}
