6ad87a9c9bb29b649122ff90b1f2ee328b7afaed
Andrew Tran <tandrew468@gmail.com>
2022-01-11 13:14:11 +1100

    Added to utilities helper method to check if string is an int
    Signed-off-by: Andrew Tran <tandrew468@gmail.com>

--- Assignment1/Utilities.cs
+++ Assignment1/Utilities.cs
@@ -8,5 +8,21 @@ public static class Utilities
     {
         return PBKDF2.Verify(passwordHash, password);
     }
-    
+
+    public static bool CheckStringIsAnInt(string s)
+    {
+        switch (s)
+        {
+            case null:
+            case "":
+                return false;
+        }
+
+        return int.TryParse(s, out var i);
+    }
+
+    public static int ConvertToInt32(string s)
+    {
+        return int.Parse(s);
+    }
 }
