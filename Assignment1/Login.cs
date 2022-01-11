75aa56ca60d2e9c403dae7a4637a7ddcf544d5e6
Andrew Tran S3785952 <tandrew468@gmail.com>
2022-01-11 11:06:39 +1100

    Added login feature
    Signed-off-by: Andrew Tran <tandrew468@gmail.com>

---
+++ Assignment1/Login.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Main;
+
+public abstract class Login
+{
+    public static void LoginMenu()
+    {
+        var rgx = new Regex("^[0-9]{8}$");
+
+        Console.WriteLine("Welcome to Most Common Bank of Australia (MCBA) \n" +
+                          "To get started enter your credentials below");
+        Console.Write("Enter Login ID: ");
+        var loginId = Console.ReadLine();
+
+        var loginIdEightDigitsLong = rgx.IsMatch(loginId);
+
+        Console.Write("Enter Password: ");
+        var password = Utilities.ReadPassword();
+        var passwordEightDigitsLong = rgx.IsMatch(password);
+
+
+        // logic to check with sql server if they match or not
+        // or pass into another class Utilities loginId and password get
+        // password hash from db from loginId and if they match return true 
+        // else return false 
+
+        // within this method also pass in loginIdEightDigitsLong and passwordEightDigitsLong 
+        // return true / false based on request
+        const bool validLoginDetails = true;
+
+        switch (validLoginDetails)
+        {
+            case true:
+                break;
+            case false:
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid LoginID and / or Password. \n " +
+                                  "Please Check Your Details And Try Again");
+                Console.ResetColor();
+                break;
+        }
+    }
+}
